00e6faf5ee201746c8329e014d038375f1abd239
Sean Mahan <sean97140@gmail.com>
2021-11-26 19:36:52 -0800

    python 2.7 to 3 conversion

--- scripts/blue_green_flip.py
+++ scripts/blue_green_flip.py
@@ -6,7 +6,7 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import print_function
+
 
 import json
 import boto3
